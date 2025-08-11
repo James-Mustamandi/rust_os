e1c3bae285dbd1e0f71966802c4e148a68cc2704
James Mustamandi <james.mustamandi@gmail.com>
2025-08-11 14:31:29 -0700

    initial commit

---
+++ target/debug/deps/rust_os-91f515180e588d9f.d
@@ -0,0 +1,5 @@
+/home/jmustama/learning/rust/rust_os/target/debug/deps/rust_os-91f515180e588d9f.d: src/main.rs
+
+/home/jmustama/learning/rust/rust_os/target/debug/deps/librust_os-91f515180e588d9f.rmeta: src/main.rs
+
+src/main.rs:
---
+++ target/debug/deps/rust_os-b8e33f7b6ead0a77.d
@@ -0,0 +1,5 @@
+/home/jmustama/learning/rust/rust_os/target/debug/deps/rust_os-b8e33f7b6ead0a77.d: src/main.rs
+
+/home/jmustama/learning/rust/rust_os/target/debug/deps/librust_os-b8e33f7b6ead0a77.rmeta: src/main.rs
+
+src/main.rs:
