07beaeba24df8880e4031c0582fb7148079a81ed
James Mustamandi <james.mustamandi@gmail.com>
2025-08-11 16:44:18 -0700

    Finish bootload with qemu system x86

--- src/main.rs
+++ src/main.rs
@@ -3,14 +3,25 @@
 
 use core::panic::PanicInfo;
 
-#[unsafe(no_mangle)] // don't mangle the name of this function
-pub extern "C" fn _start() -> ! {
-    // This is where the code starts
-    loop {}
-}
-
 /// This function is called on panic.
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
+    loop {}
+}
+
+// global, immutable reference to a static slice of bytes containing the ASCII values for Hello World!
+static HELLO: &[u8] = b"Hello World!";
+
+#[unsafe(no_mangle)]
+pub extern "C" fn _start() -> ! {
+    let vga_buffer: *mut u8 = 0xb8000 as *mut u8;
+
+    for (i, &byte) in HELLO.iter().enumerate() {
+        unsafe {
+            *vga_buffer.offset(i as isize * 2) = byte;
+            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
+        }
+    }
+
     loop {}
 }
