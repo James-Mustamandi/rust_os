de09b2d4f9cd2fe7050b6c9c7d7608c29310ba3d
James Mustamandi <james.mustamandi@gmail.com>
2025-08-18 12:55:34 -0700

    setup basic test framework

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,24 @@
 #![no_std] // no standard library
 #![no_main] // disable all Rust-level entry points
+
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test_runner)]
+#![reexport_test_harness_main = "test_main"]
+
+#[cfg(test)]
+pub fn test_runner(tests: &[&dyn Fn()]) {
+    println!("Running {} tests", tests.len());
+    for test in tests {
+        test();
+    }
+}
+
+
 mod vga_buffer;
 
 
 use core::panic::PanicInfo;
+
 
 /// This function is called on panic.
 #[panic_handler]
@@ -18,5 +33,15 @@ fn panic(info: &PanicInfo) -> ! {
 pub extern "C" fn _start() -> ! {
     println!("Hello World{}", "!");
 
+    #[cfg(test)]
+    test_main();
+
     loop {}
+}
+
+#[test_case]
+fn trivial_assertion() {
+    print!("trivial assertion... ");
+    assert_eq!(1, 1);
+    println!("[ok]");
 }
